570c1c5bac6a7d6600b88e806ee7f50762246284
thancoder <thancoder2019@gmail.com>
2025-02-08 14:11:50 +0630

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:novel_v3/app/my_app.dart';
 import 'package:novel_v3/app/utils/config_util.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:window_manager/window_manager.dart';
 
 void main() async {
